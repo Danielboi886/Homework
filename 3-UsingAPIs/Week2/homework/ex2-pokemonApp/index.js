51275ac25e160b59eec099ae222b7b9f70d56941
Daniel Wang <Daniel.y.wang95@gmail.com>
2021-12-15 01:57:36 +0100

    ex3 version 2. With better code clearity.

--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -22,76 +22,76 @@ Use async/await and try/catch to handle promises.
 Try and avoid using global variables. As much as possible, try and use function 
 parameters and return values to pass data back and forth.
 ------------------------------------------------------------------------------*/
-const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
-
-async function fetchData() {
+async function fetchData(url) {
   const response = await fetch(url);
   if (!response.ok) {
-    throw new Error('ERROR');
+    throw new Error('Sorry please try again');
   }
-  const responseData = await response.json();
-  return responseData;
-}
-async function fetchAndPopulatePokemons(data) {
-  const dataArray = data.results;
-  const pokemonMenu = document.getElementById('pokemon-menu');
-  dataArray.forEach((pokemon) => {
-    const name = pokemon.name;
-    const pokemonName = document.createElement('option');
-    pokemonName.value = name;
-    pokemonName.textContent = name;
-    pokemonMenu.appendChild(pokemonName);
-  });
-  pokemonMenu.addEventListener('change', (event) => {
-    const pokemonProfilePageURL = `https://pokeapi.co/api/v2/pokemon/${event.target.value}`;
-    console.log(pokemonProfilePageURL);
-    fetchImage(pokemonProfilePageURL);
-  });
+  const data = await response.json();
+  return data;
 }
 
-function createDOMelement() {
+async function fetchAndPopulatePokemons() {
+  const pokemonData = fetchData('https://pokeapi.co/api/v2/pokemon?limit=151');
+  const selectionMenu = document.createElement('select');
+  document.body.appendChild(selectionMenu);
+  selectionMenu.setAttribute('id', 'selection_menu');
+  selectionMenu.style.display = 'block';
+  selectionMenu.style.margin = '30px';
+  pokemonData.then((data) => {
+    const pokemonArray = data.results;
+    console.log(pokemonArray);
+    populatePokemonName(pokemonArray);
+  });
+  const populatePokemonName = (pokemonArray) => {
+    pokemonArray.forEach((pokemon) => {
+      const pokemonName = pokemon.name;
+      const nameHolder = document.createElement('option');
+      nameHolder.value = pokemonName;
+      nameHolder.textContent = pokemonName;
+      selectionMenu.appendChild(nameHolder);
+    });
+  };
+  const pokemonImage = document.createElement('img');
+  pokemonImage.setAttribute('id', 'pokemon_image');
+  pokemonImage.src = '';
+  pokemonImage.alt = '';
+  pokemonImage.style.display = 'block';
+  pokemonImage.style.margin = '30px';
+  document.body.appendChild(pokemonImage);
+  fetchImage();
+}
+
+async function fetchImage(url) {
+  const pokemonMenu = document.getElementById('selection_menu');
+  pokemonMenu.addEventListener('change', (event) => {
+    url = `https://pokeapi.co/api/v2/pokemon/${event.target.value}`;
+    getImageLink(url);
+  });
+  async function getImageLink(url) {
+    fetchData(url).then((pokemonProfile) => {
+      const pokemonImageURL = pokemonProfile.sprites.front_default;
+      populateImage(pokemonProfile, pokemonImageURL);
+    });
+  }
+  async function populateImage(profile, url) {
+    const pokemonImage = document.getElementById('pokemon_image');
+    pokemonImage.alt = profile.name;
+    pokemonImage.src = url;
+  }
+}
+
+async function main() {
   const button = document.createElement('button');
   button.textContent = 'Get Pokemon!';
   button.type = 'button';
   button.id = 'pokemon-button';
-  document.body.appendChild(button);
-  const pokemonMenu = document.createElement('select');
-  pokemonMenu.id = 'pokemon-menu';
-  document.body.appendChild(pokemonMenu);
-  const pokemonImagePlaceHolder = document.createElement('img');
-  pokemonImagePlaceHolder.id = 'pokemon-image';
-  pokemonImagePlaceHolder.alt = '';
-  pokemonImagePlaceHolder.src = ' ';
-  document.body.appendChild(pokemonImagePlaceHolder);
   button.style.display = 'block';
   button.style.margin = '30px';
-  pokemonMenu.style.display = 'block';
-  pokemonMenu.style.margin = '30px';
-  pokemonImagePlaceHolder.style.display = 'block';
-  pokemonImagePlaceHolder.style.margin = '30px';
-}
-
-async function fetchImage(url) {
-  const response = await fetch(url);
-  const responseData = await response.json();
-  console.log(responseData);
-  const imageURL = responseData.sprites.front_default;
-  const pokemonImage = document.getElementById('pokemon-image');
-  pokemonImage.alt = responseData.name;
-  pokemonImage.src = imageURL;
-}
-
-async function main() {
-  createDOMelement();
-  try {
-    const pokemonButton = document.getElementById('pokemon-button');
-    const result = await fetchData();
-    pokemonButton.addEventListener('click', function () {
-      fetchAndPopulatePokemons(result);
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  document.body.appendChild(button);
+  button.addEventListener('click', function () {
+    fetchAndPopulatePokemons();
+  });
 }
 
 window.addEventListener('load', main);
